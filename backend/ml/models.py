dbc68cd1225834a7587013a8e44109a9fe638e1e
Abay Bek <bekbaganbetov.abay@gmail.com>
2018-05-28 18:00:42 +0600

    colored maps

--- backend/ml/models.py
+++ backend/ml/models.py
@@ -11,6 +11,16 @@ import numpy as np
 model = CatBoostClassifier().load_model(settings.MODEL_FILE)
 encoder = preprocessing.LabelEncoder()
 encoder.classes_ = np.load(settings.MODEL_HEADER)
+X_columns = ['distance', 'velocity', 'acceleration',
+             'v_rol', 'v_rsd', 'v_qu1', 'v_qu2', 'v_qu3',  
+             'a_rol', 'a_rsd', 'a_qu1', 'a_qu2', 'a_qu3']
+
+colors_map = {
+    '0.0': 'red',
+    '1.0': 'green',
+    '2.0': 'yellow',
+    '3.0': 'black'
+}
 
 # Create your models here.
 
@@ -27,16 +37,37 @@ class GpsData(models.Model):
         return os.path.basename(self.data.name)
     
     def get_map(self):
+        def predict(df):
+            ans = model.predict(df[X_columns])
+            df['ans'] = ans
+            return df
+            
         def get_lat_lon(df):
             outArray = []
+            lastAns = -1
             for i, val in df.iterrows():
-                outArray.append((val.lat, val.long))
+                c = colors_map[str(val.ans)]
+                if lastAns == -1:
+                    tmp = {c: []}
+                    lastAns = val.ans
+                if val.ans != lastAns:
+                    outArray.append(tmp)
+                    tmp = {c: [(val.lat, val.long)]}
+                    lastAns = val.ans
+                elif val.ans == lastAns:
+                    tmp[c].append((val.lat, val.long))
             return outArray
 
         def write_map(outArray):
-            gmap = gmplot.GoogleMapPlotter(outArray[0][0], outArray[0][1], 13, 'AIzaSyAmwiUhqobHQDom0pYa8mdwR8nx7y6GvlM')
-            golden_gate_park_lats, golden_gate_park_lons = zip(*outArray)
-            gmap.plot(golden_gate_park_lats, golden_gate_park_lons, 'cornflowerblue', edge_width=10)
+            first = True
+            for cArray in outArray:
+                for key, value in cArray.items():
+                    golden_gate_park_lats, golden_gate_park_lons = zip(*value)
+                    if first:
+                        gmap = gmplot.GoogleMapPlotter(golden_gate_park_lats[0], golden_gate_park_lons[0], 13, 'AIzaSyAmwiUhqobHQDom0pYa8mdwR8nx7y6GvlM')
+                        first = False
+                    gmap.plot(golden_gate_park_lats, golden_gate_park_lons, key, edge_width=10)
+
             gmap.draw("my_map.html")
             with open('my_map.html', 'r') as myfile:
                 data=myfile.read()
@@ -44,6 +75,8 @@ class GpsData(models.Model):
 
         def preprocess(datapath):
             df = load_trajectory_df(datapath)
+            df = predict(df)
+
             array = get_lat_lon(df)
             html = write_map(array)
             return html
